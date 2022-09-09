03c3770af118315fa088a486f037f438242e88ef
fsalcedo55 <fsalcedo55@hotmail.com>
2022-09-09 19:53:57 -0400

    Configure packages

---
+++ dreamvision-ai/client/src/components/Loading/index.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const LoadingComponent = () => {
+  return (
+    <div className="wrapper">
+      <div className="spinner">
+        <div className="bounce1"></div>
+        <div className="bounce2"></div>
+        <div className="bounce3"></div>
+      </div>
+    </div>
+  )
+}
+
+export default LoadingComponent
---
+++ dreamvision-ai/client/src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+// import logo from "../logo.svg"
+import "../App.css"
+
+function HomePage() {
+  return <div></div>
+}
+
+export default HomePage
