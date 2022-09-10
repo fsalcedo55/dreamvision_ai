c898af51ea14a7a61233124aa1dadfaed310e02a
Julian Rivera <julianarevircoins@gmail.com>
2022-09-10 14:21:35 -0400

    dashboard

---
+++ dreamvision-ai/client/src/components/Image.jsx
@@ -0,0 +1,10 @@
+const Image = () => {
+  return (
+    <div>
+      <img src='' alt='heyhey' width='512' height='512' />
+      <p className='text-center'>Description of Image goes here</p>
+    </div>
+  );
+};
+
+export default Image;
